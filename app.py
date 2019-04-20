6c3de8ecae1d66e0a33b916b3edb5883e8d09e11
Tony Benoy <me@tonybenoy.com>
2019-04-21 02:02:14 +0530

    WIP

--- app.py
+++ app.py
@@ -1,41 +1,94 @@
-from telegram.ext import Updater, CommandHandler,MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import psutil
 import os
+import sqlite3
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-adminuserid = "You numeric user id"
+adminuserid = 138585158
 username = "tonybenoy"
-first_name = "Your First name"
-last_name = "Your lastname"
-bottoken = 'Your token'
-
-chat= {
-            'id': adminuserid,
-            'type': 'private',
+first_name = "Tony"
+last_name = "Benoy"
+bottoken = ''
+adminchat = {
+    'id': adminuserid,
+    'type': 'private',
             'username': username,
             'first_name': first_name,
             'last_name': last_name
-        }
-def start(bot, update):
-	update.message.reply_text("I'm a bot, Nice to meet you!")
+}
 
-def test(bot,update):
-	update["message"]["chat"]["username"]
-	if str(update["message"]["chat"]) == str(chat):
-		data="Hostname : "+str(os.uname())+"\nCPU Percent : "+str(psutil.cpu_percent(1))+"\nCPU Frequenct : "+str(psutil.cpu_freq(percpu=True))
-		update.message.reply_text(data)
-	else:
-		update.message.reply_text("You are not in Admin List you Hacker! XD")
-		
-	
+
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="Sorry, I didn't understand that command.")
+    bot.send_message(chat_id=adminuserid, text=str(update))
+
+
+def start(bot, update):
+    if str(update["message"]["chat"]) == str(adminchat):
+        print(bot)
+        update.message.reply_text(
+            "Hi "+update.message.from_user.first_name+"!")
+    else:
+        update.message.reply_text("I'm a Tony's Bot, Nice to meet you" +
+                                  update.message.from_user.first_name+"! But has Tony asked you to use me?")
+        bot.send_message(chat_id=adminuserid, text=str(update))
+
+
+def serverstats(bot, update):
+    if str(update["message"]["chat"]) == str(adminchat):
+        data = "Hostname : " + str(os.uname())
+        update.message.reply_text(data)
+        data = "CPU Percent : " + str(psutil.cpu_percent(1))
+        update.message.reply_text(data)
+        data = "CPU Frequenct : " + str(psutil.cpu_freq(percpu=True))
+        update.message.reply_text(data)
+        data = "Memory : "+str(psutil.virtual_memory())
+        update.message.reply_text(data)
+        data = "BootTime : " + str(psutil.boot_time())
+        update.message.reply_text(data)
+        data = "Users : " + str(psutil.users())
+        update.message.reply_text(data)
+    else:
+        update.message.reply_text("You are not in Admin List you Hacker! XD")
+        bot.send_message(chat_id=adminuserid, text=str(update))
+        #bot.send_photo(chat_id=update.message.chat_id, photo=open(str(os.path.dirname(os.path.realpath(__file__)))+'/hacker.gif', 'rb'))
+
+
+def serverstatjob(bot, update):
+    data = "Hostname : " + str(os.uname())
+    bot.send_message(chat_id=adminuserid, text=data)
+    data = "CPU Percent : " + str(psutil.cpu_percent(1))
+    bot.send_message(chat_id=adminuserid, text=data)
+    data = "CPU Frequenct : " + str(psutil.cpu_freq(percpu=True))
+    bot.send_message(chat_id=adminuserid, text=data)
+    data = "Memory : "+str(psutil.virtual_memory())
+    bot.send_message(chat_id=adminuserid, text=data)
+    data = "BootTime : " + str(psutil.boot_time())
+    bot.send_message(chat_id=adminuserid, text=data)
+    data = "Users : " + str(psutil.users())
+    bot.send_message(chat_id=adminuserid, text=data)
+
+
+def jot(bot, update):
+    if str(update["message"]["chat"]) == str(adminchat):
+        print("TO be implemented")
+
+
 def main():
-	updater = Updater(bottoken)
-	dp = updater.dispatcher
-	dp.add_handler(CommandHandler('test',test))
-	start_handler = CommandHandler('start', start)
-	dp.add_handler(start_handler)
-	updater.start_polling()
-	updater.idle()
+    updater = Updater(bottoken)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('serverstats', serverstats))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('jot', jot))
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    updater.start_polling()
+    j = updater.job_queue
+    job_hourly = j.run_repeating(serverstatjob, interval=86400, first=0)
+    updater.idle()
+
 
 if __name__ == '__main__':
-	main()
-
+    main()
